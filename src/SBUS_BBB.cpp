ed3a5d8e7f75c387721d358d6bfc14b6a20a940d
Mark Vandermeulen <majik_mark@hotmail.com>
2016-04-10 21:54:28 -0400

    Local serial test works

--- src/SBUS_BBB.cpp
+++ src/SBUS_BBB.cpp
@@ -18,21 +18,72 @@ using namespace std;
 
 #include "blacklib/BlackLib.h"
 #include "blacklib/BlackUART.h"
+#include "blacklib/BlackTime.h"
+//#include "blacklib/BlackCore.h"
 
 int main() {
 
+	//start a timer to measure runtime of various functions.
+	 BlackLib::BlackTime mytime;   //Timer from the BlackLib library
+	 BlackLib::BlackTimeElapsed et; //Counts the elapsed time from the timer
+	 mytime.start();
+
 	cout << "!!!Hello World!!2?!" << endl; // prints !!!Hello World!!?!
 
+	//create 2 UART serial connections. Wire them together
+	//According to the pinout (http://beagleboard.org/support/bone101), UART1_TXD = P9_24, UART1_RXD = P9_26. UART2_TXD = P9_21, UART2_RXD = P9_22
+	//  EDIT: UART2 is disabled because I am using it for SPI (required funny configuration).
+	//	Instead of UART2, use UART4. UART4_RXD = P9_11, UART4_TXD = P9_13
+	//Connect the RX to TX. Wiring goes like this:
+	//				(UART4_TXD)	P9_13 -> P9_26 (UART1_RXD)
+	//				(UART4_RXD)	P9_11 -> P9_24 (UART1_TXD)
 	BlackLib::BlackUART  myUart(BlackLib::UART1,
 	                                       BlackLib::Baud9600,
 	                                       BlackLib::ParityEven,
 	                                       BlackLib::StopOne,
 	                                       BlackLib::Char8 );
-	BlackLib::BlackUART myUart2(BlackLib::UART2,
+	BlackLib::BlackUART myUart2(BlackLib::UART4,
 											BlackLib::Baud9600,
 											BlackLib::ParityEven,
 											BlackLib::StopOne,
 											BlackLib::Char8 );
+	//opend the UART connections
+	myUart.open( BlackLib::ReadWrite | BlackLib::NonBlock);
+	myUart2.open( BlackLib::ReadWrite | BlackLib::NonBlock);
+
+	cout << "UART2 status: " <<  myUart2.isOpen() << endl;
+
+	//write a test message
+	string testMessage = "Test UART message";
+	//flush the send and receive buffers
+	myUart2.flush( BlackLib::bothDirection );
+	myUart.flush( BlackLib::bothDirection );
+	//write the message to UART1
+	cout << myUart.write(testMessage);
+	usleep(5000); //wait 50 ms. You have to wait for the message to be sent. at 9600 baud, sends about 1 char per ms.
+
+
+	cout << "Message sent: " << testMessage << endl;
+	//cout << "message received: " <<  myUart2.read() << endl;
+	//read from the UART buffer. For implementation, it is best to put this in a separate thread because of the long waits between data.
+	char readBuffer[testMessage.length()];
+	char buf[1]; //single buffer
+	int i = 0;
+	while(i < testMessage.length()){
+		//myUart2.read(char* buf, size) takes 8 microseconds if no message, 10 microseconds if message.
+		if(myUart2.read(buf, sizeof(buf))){
+			readBuffer[i] = buf[0];
+			i ++;
+		}else{
+			usleep(500); //wait 0.5 ms if nothing is available to read
+		}
+	}
+	cout << "Message received: " <<  readBuffer << endl;
+
+
+	//close the UART connecctions
+	myUart.close();
+	myUart2.close();
 
 	cout << "LED Flash Start" << endl;
 
